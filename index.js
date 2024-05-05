6709ecef6338edae539303736e57e668388d1981
rubenrafael1983@gmail.com <92895134+rubenrafael1983@users.noreply.github.com>
2024-05-05 01:54:23 -0300

    clase 1

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hola Equipo")
+console.log("Hola Equipo")
+console.log("Hola Equipo")
+console.log("Hola Equipo")
+
+let nombre= "Ruben"
+console.log("🚀 ~ nombre:", nombre)
+//clg para escribir un colsole log
+
+//hay 3 tipos de variables
+var nombre2 = "canela"; //no se utiliza // puede no tener un valor, no se declara el tipo, es no tipado
+console.log("🚀 ~ nombre2:", nombre2)
+let variable2 = "canela";
+console.log("🚀 ~ variable2:", variable2)
+const edad=32;
+console.log("🚀 ~ edad:", edad)
+
